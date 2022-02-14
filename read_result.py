0fa837c40136b84fde15836698fffec50de58790
loganzhang1 <2677684261@qq.com>
2022-02-14 12:25:43 +0800

    1_14

--- read_result.py
+++ read_result.py
@@ -16,13 +16,13 @@ if __name__=='__main__':
     args=parser.parse_args()
     print('args',args)
     print('args.dataset',args.dataset)
-    data=pd.DataFrame(columns=['acc','f1'])
+    data=pd.DataFrame(columns=['seed','lr','batch_size','acc','f1'])
     save_path='./extractDataFile/{}_lr_{}_batch_size_{}.csv'.format(args.dataset, args.lr, args.batch_size)
 
     for seed in range(1234,1239):
         result_path='./model_dir/{}_lr_{}_batch_size_{}_imagenet_8_0.0001_40_fusion_2_12_{}/eval_result.csv'.format(args.dataset, args.lr, args.batch_size,seed)
         print('result_path',result_path)
-        temp=[]
+        temp=[seed, args.lr, args.batch_size]
         read=pd.read_csv(result_path)
         temp.append(float(list(read.columns)[0]))
         print(read)
